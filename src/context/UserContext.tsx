4a202290668361d41e5929d3ad07adbfb333972f
rahmanmue <rahman.muraman@gmail.com>
2025-04-25 15:16:57 +0700

    ref: handle useeffect context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { createContext, useState, useEffect, ReactNode } from "react";
 import { getDetailUser } from "@/services/users";
+import { usePathname } from "next/navigation";
 
 export type User = {
+  id: string;
   username: string;
   fullname: string;
   role: string;
@@ -18,19 +20,24 @@ export const UserContext = createContext<UserContextType | undefined>(
 );
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
   const [user, setUser] = useState<User | null>(null);
+  const fetchUser = async () => {
+    try {
+      const res = await getDetailUser();
+      setUser(res.data);
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await getDetailUser();
-        setUser(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUser();
-  }, []);
+    if (pathname.includes("group-tasks") && user === null) {
+      fetchUser();
+    }
+  }, [pathname]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
